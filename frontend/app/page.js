3d7e4e191e6a97182c03062808d71aa551702549
Rounak Bhatia <rounak@contlo.com>
2024-03-21 23:54:22 +0530

    additions in frontend

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -1,14 +1,18 @@
-'use client';
+"use client";
 
-import React, { useEffect, useState } from "react";
-import Image from "next/image";
+import React, { useState } from "react";
 import styles from "./page.module.css";
-import { fetchItemByName, updateItem, deleteItem, issueItem, returnItem } from "@/pages/api/DashboardService";
+import { fetchItemByName, issueItem } from "@/pages/api/DashboardService";
 
 export default function Home() {
-  const [searchTerm, setSearchTerm] = useState('');
+  const [searchTerm, setSearchTerm] = useState("");
   const [item, setItem] = useState(null);
   const [error, setError] = useState(null);
+  const [itemNameInput, setItemNameInput] = useState("");
+  const [studentIdInput, setStudentIdInput] = useState("");
+  const [studentNameInput, setStudentNameInput] = useState("");
+  const [notificationVisible, setNotificationVisible] = useState(false);
+  const [notificationMessage, setNotificationMessage] = useState("");
 
   const handleSearch = async () => {
     try {
@@ -16,8 +20,8 @@ export default function Home() {
       setItem(item);
       setError(null);
     } catch (error) {
-      console.error('Error fetching item:', error);
-      setError('An error occurred while fetching the item.');
+      console.error("Error fetching item:", error);
+      setError(error.response?.data.detail || "An error occurred");
       setItem(null);
     }
   };
@@ -26,8 +30,34 @@ export default function Home() {
     setSearchTerm(e.target.value);
   };
 
+  const handleIssueItem = async () => {
+    try {
+      const data = {
+        item_name: itemNameInput,
+        student_id: studentIdInput,
+        student_name: studentNameInput,
+      };
+      await issueItem(data);
+      setNotificationMessage("Item issued successfully!");
+      setNotificationVisible(true);
+    } catch (error) {
+      console.error("Error issuing item:", error);
+      setNotificationMessage(error.response?.data.detail || "An error occurred while issuing the item!");
+      setNotificationVisible(true);
+    }
+    setItemNameInput("");
+    setStudentIdInput("");
+    setStudentNameInput("");
+    setTimeout(() => {
+      setNotificationVisible(false);
+      setNotificationMessage("");
+    }, 3000);
+  };
+
   return (
-      <div className={styles.container}>
+    <div className={styles.container}>
+      <h1 className={styles.heading}>Inventory Management System</h1>
+      <h2 className={styles.subHeading}>Search Items</h2>
       <div className={styles.formContainer}>
         <input
           type="text"
@@ -43,7 +73,7 @@ export default function Home() {
           <button
             className={styles.clearButton}
             onClick={() => {
-              setSearchTerm('');
+              setSearchTerm("");
               setItem(null);
               setError(null);
             }}
@@ -55,10 +85,44 @@ export default function Home() {
       {error && <p className={styles.errorMessage}>{error}</p>}
       {item && (
         <div className={styles.itemDetails}>
-          {/* Render item details */}
-          <pre>{JSON.stringify(item, null, 2)}</pre>
+          <p>
+            {item.item_name.toUpperCase()} with ID:{item.item_id} has not been
+            issued to anyone.
+          </p>
         </div>
       )}
-    </div>   
+      <h2 className={styles.subHeading}>Issue Items</h2>
+      <div className={styles.issueForm}>
+        <input
+          type="text"
+          className={styles.searchInput}
+          placeholder="Enter Item Name"
+          value={itemNameInput}
+          onChange={(e) => setItemNameInput(e.target.value)}
+        />
+        <input
+          type="text"
+          className={styles.searchInput}
+          placeholder="Enter Student ID"
+          value={studentIdInput}
+          onChange={(e) => setStudentIdInput(e.target.value)}
+        />
+        <input
+          type="text"
+          className={styles.searchInput}
+          placeholder="Enter Student Name"
+          value={studentNameInput}
+          onChange={(e) => setStudentNameInput(e.target.value)}
+        />
+        <div className={styles.buttonContainer}>
+          <button className={styles.searchButton} onClick={handleIssueItem}>
+            Issue Item
+          </button>
+        </div>
+        {notificationVisible && (
+          <div className={styles.notification}>{notificationMessage}</div>
+        )}
+      </div>
+    </div>
   );
 }
